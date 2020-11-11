5cc858cd95672a35f95c5e8836bd7f38715a4225
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-11-11 14:25:16 +0100

    Added new Oppenent Ship

--- core/src/com/spacetric/gameexample/Game.java
+++ core/src/com/spacetric/gameexample/Game.java
@@ -3,22 +3,23 @@ package com.spacetric.gameexample;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.spacetric.gameexample.moveable.objects.SpaceObject;
+import com.spacetric.gameexample.moveable.ships.OpponentShip;
 
 public class Game extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
     SpaceObject meteor;
+    OpponentShip opponentShip;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
 
         meteor = new SpaceObject("Astroids/Astroid1_1.png", 2, 200, 200, 20, Gdx.graphics.getHeight());
-
+        opponentShip = new OpponentShip("Ships/opShip1.png", 6, 200, 200, 40, Gdx.graphics.getHeight());
     }
 
     @Override
@@ -28,7 +29,7 @@ public class Game extends ApplicationAdapter {
         batch.begin();
         try {
             meteor.moveDown(batch);
-
+            opponentShip.moveDown(batch);
         } catch (Exception e) {
             System.out.println("YOU LOST");
         }
