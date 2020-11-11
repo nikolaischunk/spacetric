e8c0e5d07388aa4f717140d2fab9ac9e5c400d9d
Stefan Hehlen <65352278+TH3Z0DIAC@users.noreply.github.com>
2020-11-11 16:21:53 +0100

    added getter and setter for x and y

--- core/src/com/spacetric/gameexample/abstraction/SpaceElement.java
+++ core/src/com/spacetric/gameexample/abstraction/SpaceElement.java
@@ -45,4 +45,19 @@ public abstract class SpaceElement {
         return img;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
