bdba461973ee43a556abcddccca6f119887f2559
Stefan Hehlen <65352278+TH3Z0DIAC@users.noreply.github.com>
2020-11-11 16:17:27 +0100

    changed Game so its runnable

--- core/src/com/spacetric/gameexample/Game.java
+++ core/src/com/spacetric/gameexample/Game.java
@@ -21,7 +21,7 @@ public class Game extends ApplicationAdapter {
         batch = new SpriteBatch();
         meteor = new SpaceObject("Astroids/Astroid1_1.png", 2, 200, 200, 20, Gdx.graphics.getHeight());
         opponentShip = new OpponentShip("Ships/opShip1.png", 6, 200, 200, 40, Gdx.graphics.getHeight());
-        myShip = new OwnShip("Ships/playerShip.png", 0, 150, 150, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/8, "Shots/Shot1/shot1_1.png");
+        myShip = new OwnShip("Ships/playerShip.png", 0, Gdx.graphics.getWidth()/3, Gdx.graphics.getHeight()/8, "Shots/Shot1/shot1_1.png");
     }
 
     @Override
