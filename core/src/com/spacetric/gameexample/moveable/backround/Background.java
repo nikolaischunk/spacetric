f201caec1bb7fc260ea9a06fa2c7196ba27cf58c
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-11-12 14:03:39 +0100

    Update Background.java

--- core/src/com/spacetric/gameexample/moveable/backround/Background.java
+++ core/src/com/spacetric/gameexample/moveable/backround/Background.java
@@ -8,25 +8,33 @@ import com.badlogic.gdx.math.Rectangle;
 public class Background extends Rectangle {
 
     Texture sky;
-    float secondBackgroundY;
+    Texture sky2;
+    int ysky;
+    int ysky2;
 
     /**
      * Constructs a new rectangle with all values set to zero
      */
     public Background(Texture sky) {
         this.sky = sky;
-        this.secondBackgroundY = Gdx.graphics.getHeight();
+        this.ysky = 0;
+
+        this.ysky2 = sky.getHeight();
     }
 
     public void render(Batch batch) {
 
-        batch.draw(sky, x, y, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        y = --y;
-        float topBg1 = y + sky.getHeight();
-        if (topBg1 < 0) {
-            //Draw Second Background
-            batch.draw(sky, x, secondBackgroundY, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            Gdx.app.log("BACKGROUND", topBg1 + "");
+        ysky = --ysky;
+        ysky2 = -- ysky2;
+        float topBg1 = ysky + sky.getHeight();
+        float topBg2 = ysky2 + getHeight();
+        if(topBg1 < 0){
+            ysky = (int)topBg2;
         }
+        if (topBg2 < 0){
+            ysky2 = (int)topBg1;
+        }
+        batch.draw(sky, x, ysky, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(sky, x, ysky2, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 }
