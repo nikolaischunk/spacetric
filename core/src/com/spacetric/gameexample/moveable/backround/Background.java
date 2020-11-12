0a1a76219b5955787e513b22f09051f9fcc298bb
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2020-11-12 14:59:13 +0100

    fixed hopping bug and stuck drawings

--- core/src/com/spacetric/gameexample/moveable/backround/Background.java
+++ core/src/com/spacetric/gameexample/moveable/backround/Background.java
@@ -9,32 +9,48 @@ public class Background extends Rectangle {
 
     Texture sky;
     Texture sky2;
-    int ysky;
-    int ysky2;
+    float ysky;
+    float ysky2;
+    float secondBackgroundY;
+    float speed;
 
     /**
      * Constructs a new rectangle with all values set to zero
      */
-    public Background(Texture sky) {
+    public Background(Texture sky, Texture sky2) {
         this.sky = sky;
+        this.sky2 = sky2;
         this.ysky = 0;
-
         this.ysky2 = sky.getHeight();
+
+        this.speed = 250;
+
+
+        this.secondBackgroundY = Gdx.graphics.getHeight();
+
     }
 
     public void render(Batch batch) {
 
-        ysky = --ysky;
-        ysky2 = -- ysky2;
-        float topBg1 = ysky + sky.getHeight();
-        float topBg2 = ysky2 + getHeight();
-        if(topBg1 < 0){
-            ysky = (int)topBg2;
+        ysky = ysky - (speed * Gdx.graphics.getDeltaTime());
+        ysky2 = ysky2 - (speed * Gdx.graphics.getDeltaTime());
+
+        float width = Gdx.graphics.getWidth();
+        float height = (width / sky.getWidth()) * sky.getHeight();
+
+        float topBg1 = ysky + height;
+        float topBg2 = ysky2 + height;
+        //Wenn erster BG aus dem Bild, position x wieder auf höhe screen setzten.
+
+        if (topBg1 < 0) {
+            ysky = Gdx.graphics.getHeight();
         }
-        if (topBg2 < 0){
-            ysky2 = (int)topBg1;
+        if (topBg2 < 0) {
+            ysky2 = Gdx.graphics.getHeight();
         }
-        batch.draw(sky, x, ysky, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(sky, x, ysky2, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        batch.draw(sky, x, ysky, width, height);
+        batch.draw(sky, x, ysky2, width, height);
     }
+
 }
